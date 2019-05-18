b5e973cf3989ff22f3172462a694192647e8cbcb
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-17 21:18:01 -0700

    feature-branch1, has seeding script and first express route

---
+++ database/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//var db = mongoose.connect("mongodb://localhost/herodb");
+var connection = mongoose.createConnection("mongodb://localhost/herodb");
+
+const gameItem = new Schema(
+  {
+    gameId:Number,
+    gameTitle:String,
+    gameDescription:String,
+    gameDeveloper:String,
+    gamePublisher:String,
+    releaseDate:Date,
+    metaTags:Array
+  }
+);
+var Games = connection.model('Game', gameItem);
+let query = (idNumber)=>{
+return Games.find({gameId:idNumber}).exec().then((game)=>{
+  return game;
+})
+}
+module.exports.query = query;
+module.exports.Games= Games;
