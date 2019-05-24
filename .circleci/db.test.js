cd681b65792ed7880f615a751f6c4f70c8ca2171
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-24 09:08:47 -0700

    added the first test, and it should pass

--- .circleci/db.test.js
+++ .circleci/db.test.js
@@ -4,8 +4,11 @@ describe('populate', ()=>{
   beforeAll(async ()=>{
     connection = await mongoose.connect("mongodb://localhost/herodb");
   });
-  expect(1).toBe(1);
   afterAll(async ()=>{
     await mongoose.disconnect();
+  });
+
+  it('should be the first test', async ()=>{
+    expect(1).toEqual(1);
   })
 })
