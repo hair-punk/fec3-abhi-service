d82e066a496df22fc05daa3f70dc76a5832cdada
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-24 15:04:42 -0700

    replaced arrow function with standard functions

--- .circleci/db.test.js
+++ .circleci/db.test.js
@@ -1,14 +1,14 @@
 const mongoose =require('mongoose');
 
 describe('populate', ()=>{
-  beforeAll(async ()=>{
+  beforeAll(async function(){
     connection = await mongoose.connect("mongodb://localhost/herodb");
   });
-  afterAll(async ()=>{
+  afterAll(async function(){
     await mongoose.disconnect();
   });
 
-  it('should be the first test', async ()=>{
+  it('should be the first test', async function(){
     expect(1).toEqual(1);
   })
 })
