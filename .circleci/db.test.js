4fabc2ba533ac09b90441dd086bd2e687c6fd59d
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-26 03:25:25 -0700

    added a test for first database query tests

--- .circleci/db.test.js
+++ .circleci/db.test.js
@@ -14,7 +14,9 @@ describe('populate', ()=>{
     expect(1).toEqual(1);
   })
   it('should test the database and return the first item', async function(){
+    var firstItem = dbQuery.query(1);
     expect(typeof await dbQuery.query(1)).toEqual('object');
+    expect(firstItem).toHaveProperty('gameId');
   })
 
   it('should test server get /test', async function(){
