e7c8c2ecd8c496c82450020410f80b86cccbd50e
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-27 16:57:36 -0700

    added a scrollbar package and implemented a basic one, needs configuration and customization

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactPlayer from 'react-player'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+
 
 class Player extends React.Component {
   constructor(props){
@@ -8,7 +10,10 @@ class Player extends React.Component {
     this.state={};
   }
   render(){
-    return (<ReactPlayer url="https://www.youtube.com/watch?v=30noyHLKLl4" playing />)
+    return (<div>
+    <ReactPlayer url="https://s3-us-west-1.amazonaws.com/exhaust-media-test/100.mp4" playing />
+    <PerfectScrollbar><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /><img src="https://s3-us-west-1.amazonaws.com/exhaust-media-test-2/1.jpg" alt="Smiley face" height="70" width="70" /></PerfectScrollbar>
+    </div>)
   }
 }
 ReactDOM.render(<Player />, document.getElementById("player"));
