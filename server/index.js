6c0f3f96741771de199784ddeac2a795ffdccbfc
chokoreetosan <mallepalliabhishek@gmail.com>
2019-05-27 21:22:40 -0700

    express server now queries amazon s3 using credentials for a single file's data

--- server/index.js
+++ server/index.js
@@ -3,15 +3,35 @@ const bodyParser = require('body-parser');
 const query = require('../database/index.js')
 const AWS = require('aws-sdk');
 let port = 3007;
-
-
+const keys = require('../.aws/credentials.js')
 let app = express();
+AWS.config.update({
+    accessKeyId:keys.AWS_ACCESS_KEY,
+    secretAccessKey:keys.AWS_SECRET_KEY
+});
+var s3 = new AWS.S3();
+var videoparams ={
+  Bucket:keys.VIDEO_BUCKET,
+  Key:''
+};
+var photoparams = {
+  Bucket:keys.PHOTO_BUCKET,
+  Key:''
+}
 app.use(express.static(__dirname + '/../dist'));
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 
 app.get('/gameObject', async function(req,res){
+  videoparams.Key = req.query.id+'.mp4'
+  var request =s3.getObject(videoparams);
+  var object = request.promise();
+  object.then((data)=>{console.log(data)}).catch((err)=>{
+    console.log('error')
+  })
+
+  // console.log(object);
   res.send(await query.query(req.query.id));
 })
 
