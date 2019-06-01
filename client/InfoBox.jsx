472d6bb122d03af9a834426779795bcdef64a3ad
chokoreetosan <mallepalliabhishek@gmail.com>
2019-06-01 00:11:01 -0700

    partial refacto done

--- client/InfoBox.jsx
+++ client/InfoBox.jsx
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 
 class InfoBox extends React.Component{
 render(){
-  return(<span style={{height:"400px", width:"300px", display:"flex", flexDirection:"column", alignItems:"center" }}>
+  return(<span style={{height:"426px", width:"300px", display:"flex", flexDirection:"column", alignItems:"center" }}>
       <div><img src={this.props.picture} height="160px" width="240px"/></div>
       <p style={{fontFamily:"arial",fontSize:"11px", alignSelf:"center"}}>{this.props.description}</p>
   </span>)
